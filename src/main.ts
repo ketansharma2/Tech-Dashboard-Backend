dd43dc37723cba9384de4c777571365252b89c29
developer-maven-02 <jrdeveloper.mavenjobs@gmail.com>
2026-06-29 14:34:17 +0530

    origin url chnage

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:5678',
+    origin: 'https://tech-dashboard-frontend.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
